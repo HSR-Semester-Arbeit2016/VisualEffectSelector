5b92090924a34986f608aafae3664b1b1586cbb9
Roberto Cuervo <rcuervoa@hsr.ch>
2016-10-21 00:05:14 +0200

    Added Red light scene
    Signed-off-by: Roberto Cuervo <rcuervoa@hsr.ch>

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+public class Test : MonoBehaviour {
+
+	public Material mat;
+	void OnRenderImage(RenderTexture src, RenderTexture dest) {
+		List<RenderTexture> sources = new List<RenderTexture>();
+		sources.Add (src);
+		if (sources.Count == 5) {
+			//Thread.Sleep(2000);
+			foreach (RenderTexture element in sources) {
+				Graphics.Blit (src, dest, mat);
+			}
+		}
+	}
+}
