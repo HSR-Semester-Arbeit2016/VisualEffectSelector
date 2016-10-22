74e83a00540c6bbb0a2544e84c85d281df6b0bb5
Roberto Cuervo <rcuervoa@hsr.ch>
2016-10-22 16:27:21 +0200

    Added alcohol level text to blur scene
    Signed-off-by: Roberto Cuervo <rcuervoa@hsr.ch>

--- Assets/Scripts/BlurViewModel.cs
+++ Assets/Scripts/BlurViewModel.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.UI;
+using System;
 using UnityStandardAssets.ImageEffects;
 
 public class BlurViewModel : MonoBehaviour {
-	
+	private readonly String soberLevel ="Sober";
+	private readonly String intoxicatedLevel = "Slightly intoxicated";
+	private readonly String drunkLevel = "Drunk";
+	private readonly string veryDrunkLevel = "Very drunk";
+
 	public void setBlurSize(float newValue) {
 		this.setBlurSizeText (newValue);
+		this.setDrunkLevelText(newValue);
 		this.setBlurSizeValue (newValue);
 	}
 
@@ -22,4 +26,28 @@ public class BlurViewModel : MonoBehaviour {
 		BlurOptimized cameraRightBlur = GameObject.Find ("StereoCameraRight").GetComponent<BlurOptimized> ();
 		cameraRightBlur.blurSize = newValue;
    }
+
+	private void setDrunkLevelText(float newValue) {
+		Text blurValueText = GameObject.Find ("SelectedAlcoholLevelText").GetComponent<Text> ();
+		String value = newValue.ToString();
+		switch (value)
+		{
+		case "0":
+			blurValueText.text = this.soberLevel;
+			break;
+		case "1":
+			blurValueText.text = this.intoxicatedLevel;
+			break;
+		case "2":
+			blurValueText.text = this.drunkLevel;
+			break;
+		case "3":
+			blurValueText.text = this.veryDrunkLevel;
+			break;
+		case "4":
+			blurValueText.text = this.veryDrunkLevel;
+			break;
+		
+		}
+	}
 }
