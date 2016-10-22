544b04de94631a48237066b02bd00934fcc35e68
Roberto Cuervo <rcuervoa@hsr.ch>
2016-10-22 16:01:13 +0200

    Modified blur scene, added BlurViewModel class
    Signed-off-by: Roberto Cuervo <rcuervoa@hsr.ch>

---
+++ Assets/Scripts/BlurViewModel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityStandardAssets.ImageEffects;
+
+public class BlurViewModel : MonoBehaviour {
+	
+	public void setBlurSize(float newValue) {
+		this.setBlurSizeText (newValue);
+		this.setBlurSizeValue (newValue);
+	}
+
+	private void setBlurSizeText(float value) {
+		Text blurValueText = GameObject.Find ("SliderValueText").GetComponent<Text> ();
+		blurValueText.text = value.ToString();
+	}
+
+	private void setBlurSizeValue(float newValue) {
+		BlurOptimized cameraLeftBlur = GameObject.Find ("StereoCameraLeft").GetComponent<BlurOptimized> ();
+		cameraLeftBlur.blurSize = newValue;
+		BlurOptimized cameraRightBlur = GameObject.Find ("StereoCameraRight").GetComponent<BlurOptimized> ();
+		cameraRightBlur.blurSize = newValue;
+   }
+}
