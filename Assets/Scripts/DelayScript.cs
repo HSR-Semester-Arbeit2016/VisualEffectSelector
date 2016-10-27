714c7a0e60e5a782907c636f1734a832b37cf986
Roberto Cuervo <rcuervoa@hsr.ch>
2016-10-27 15:58:22 +0200

    Added new delay scene
    Signed-off-by: Roberto Cuervo <rcuervoa@hsr.ch>

--- Assets/Scripts/DelayScript.cs
+++ Assets/Scripts/DelayScript.cs
@@ -3,35 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 
-public class Test : MonoBehaviour {
-	
+public class DelayScript : MonoBehaviour {
 	private Queue myQ;
-	public Test() {
+
+	public DelayScript() {
 		this.myQ = new Queue();
 
 	}	
 
-
 	void OnRenderImage(RenderTexture src, RenderTexture dest) {
-		RenderTexture temporary = RenderTexture.GetTemporary (src.width, src.height);
-
-
-		if (temporary.IsCreated ()) {
-			Debug.Log ("Yuhi ");
-			myQ.Enqueue (temporary);
-		} else {myQ.Enqueue (src);
+		if (src.IsCreated()) {
+			Debug.Log ("Source");
 		}
-
-	//gettmporary schauen
-
-
+		RenderTexture temporary = RenderTexture.GetTemporary (src.width, src.height);
+		if (temporary.IsCreated ()) {
+			Debug.Log ("Delay");
+			myQ.Enqueue (temporary);
+		} else {
+			myQ.Enqueue (src);
+		}
 		if (myQ.Count == 80) {
-			
 			src = (RenderTexture)myQ.Dequeue ();
 			Graphics.Blit(src,dest);
-		} else {
-			//	Graphics.Blit(src,dest);
-		}
-
+		} 
 	}
 }
