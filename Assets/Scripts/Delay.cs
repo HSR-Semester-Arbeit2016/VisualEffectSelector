f349e8326b25e426500be7e35e0f950ab8ef03d1
Roberto Cuervo <rcuervoa@hsr.ch>
2016-10-29 10:10:18 +0200

    Delay Scritp refactored
    Signed-off-by: Roberto Cuervo <rcuervoa@hsr.ch>

--- Assets/Scripts/Delay.cs
+++ Assets/Scripts/Delay.cs
@@ -6,23 +6,28 @@ using System.Threading;
 public class Delay : MonoBehaviour {
 	private Queue myQ;
 
+	private int delayQueueCount = 80;
+	public int DelayQueueCount {
+		get { return delayQueueCount; }
+		set {delayQueueCount = value;} 
+	}
+
 	public Delay() {
 		this.myQ = new Queue();
-
 	}	
 
 	void OnRenderImage(RenderTexture src, RenderTexture dest) {
+		RenderTexture temporary = RenderTexture.GetTemporary (src.width, src.height);
 		if (src.IsCreated()) {
 			Debug.Log ("Source");
 		}
-		RenderTexture temporary = RenderTexture.GetTemporary (src.width, src.height);
 		if (temporary.IsCreated ()) {
 			Debug.Log ("Delay");
 			myQ.Enqueue (temporary);
-		} else {
+		} else {				// we could remove this else, but I can observe a bit delay or interruption in the video when removed
 			myQ.Enqueue (src);
 		}
-		if (myQ.Count == 80) {
+		if (myQ.Count == DelayQueueCount) {
 			src = (RenderTexture)myQ.Dequeue ();
 			Graphics.Blit(src,dest);
 		} 
